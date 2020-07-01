f1aa52511dc2ed550de7ae52ccbd7386713a4c2e
Rumen Roger <rumenroger@gmail.com>
2020-07-01 13:47:55 +0100

    App.vue implemented

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,46 @@
 <template>
   <div id="app">
+    <booking-form />
+    <booking-grid :bookings="bookings" />
   </div>
 </template>
 
 <script>
-import BookingForm from './components/BookingForm';
+import BookingForm from './components/BookingForm.vue';
+import BookingsGrid from './components/BookingGrid.vue';
+import BookingService from './services/BookingService.js';
 import {eventBus} from './main.js';
 
 export default {
   name: 'App',
   components: {
-    'booking-form': BookingForm
+    'booking-form': BookingForm,
+    'booking-grid': BookingGrid
     },
-    
-    mounted(){
-    fetch("http://localhost:3000/api/bookings")
-      .then(response => response.json())
-      .then(data => console.log(data));
+
+  data() {
+    return{
+      bookings: []
+    };
   },
+
+  mounted(){
+    this.fetchBookings();
+    eventBus.$on('booking-added', booking => {
+      this.bookings.push(booking);
+    })
+
+    eventBus.$on('booking-deleted', (id) => {
+      let index = this.bookings.findIndex(booking => booking._id === id)
+      this.bookings.splice(index, 1)
+    })
+  },
+  methods: {
+    fetchBookings() {
+      BookingService.getBookings()
+      .then(bookings => this.bookings = bookings);
+    }
+  }
 }
 </script>
 
