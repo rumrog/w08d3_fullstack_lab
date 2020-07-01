aa47e25f7a3c9cddfab10ad86cc0a856838f4994
Rumen Roger <rumenroger@gmail.com>
2020-07-01 13:40:38 +0100

    BookingGrid.vue implemented

---
+++ client/src/components/BookingGrid.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="bookingGrid">
+      <booking v-for="booking in bookings" :key="booking" :booking="booking" />
+  </div>
+</template>
+
+<script>
+import Booking from './Booking.vue';
+
+export default {
+  name: 'bookings-grid',
+  components: {
+    'booking': Booking
+  },
+  props: ['bookings']
+};
+</script>
+
+<style>
+
+</style>
